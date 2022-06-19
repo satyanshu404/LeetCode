b6e12c72c166f4815b3e85115164136fff1992dc
Satyanshu kumar <67653823+satyanshu404@users.noreply.github.com>
2022-06-19 17:09:59 +0530

    Update 125. Valid Palindrome.cpp

--- 125. Valid Palindrome.cpp
+++ 125. Valid Palindrome.cpp
@@ -33,22 +33,20 @@ class Solution {
   // Time: O(n)
   
 public:
-    bool isPalindrome(string s) {
-        int i = 0;
-        int j = s.size() - 1;
+    bool isPalindrome(string s) 
+    {
+        cin.tie(0);
         
-        while (i < j) {
-            while (!isalnum(s[i]) && i < j) {
-                i++;
-            }
-            while (!isalnum(s[j]) && i < j) {
-                j--;
-            }
-            if (tolower(s[i]) != tolower(s[j])) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<=j)
+        {
+            if(!iswalnum(s[i]))
+                ++i;
+            else if(!iswalnum(s[j]))
+                --j;
+            else if(tolower(s[i++]) != tolower(s[j--]))
                 return false;
-            }
-            i++;
-            j--;
         }
         
         return true;
